241550550ab9b14b8a7c465d278bc32640fbaa98
ayomideajadi12-hub <ayomideajadi12@gmail.com>
2026-03-12 11:06:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ except Exception:
     st.stop()
 
 genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.0-flash")
 
 st.markdown("""
 <style>
